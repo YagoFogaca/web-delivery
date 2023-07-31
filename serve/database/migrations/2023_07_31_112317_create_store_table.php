5bf8f605221e3aed7c70877c8972e42a4b0fe7d1
YagoFogaca <yagofogaca20@gmail.com>
2023-07-31 08:42:29 -0300

    feat: modelagem do schema de loja

--- serve/database/migrations/2023_07_31_112317_create_store_table.php
+++ serve/database/migrations/2023_07_31_112317_create_store_table.php
@@ -6,27 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('store', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('telephone')->unique();
+            $table->string('cep');
+            $table->string('number_address');
+            $table->boolean('shop_open')->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('store');
     }
 };
