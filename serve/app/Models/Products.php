c10582857e347d56857a03c05185dc24ec25d52c
YagoFogaca <yagofogaca20@gmail.com>
2023-08-02 17:49:41 -0300

    feat: criação e implementação dos schemas e models de produtos e categorias

---
+++ serve/app/Models/Products.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'name',
+        'description',
+        'prince',
+        'image',
+        'category_id'
+    ];
+
+    public function phone(): HasOne
+    {
+        return $this->hasOne(Phone::class);
+    }
+}
