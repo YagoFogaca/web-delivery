f561a9ed2b9b5f82c61f3a66774d688791c1b2ef
YagoFogaca <yagofogaca20@gmail.com>
2023-08-03 08:10:23 -0300

    feat: implementação das validação de entrada para criar produtos

--- serve/resources/views/components/modal-create-products/index.blade.php
+++ serve/resources/views/components/modal-create-products/index.blade.php
@@ -7,23 +7,35 @@
             </div>
             <div class="modal-body">
                 <form method="POST" action={{ route('products.store') }} enctype="multipart/form-data">
+                    @error('create')
+                        <div class='invalid-feedback'><span>{{ $message }}</span></div>
+                    @enderror
                     @csrf
 
                     <div class="mb-3">
                         <label for="name" class="form-label">Nome</label>
                         <input type="text" class="form-control" id="name" name="name"
                             placeholder="X-BURGUER">
+                        @error('name')
+                            <div class='invalid-feedback'><span>{{ $message }}</span></div>
+                        @enderror
                     </div>
 
                     <div class="mb-3">
                         <label for="price" class="form-label">Preço</label>
                         <input type="text" class="form-control" id="price" name="price" placeholder="19,90">
+                        @error('price')
+                            <div class='invalid-feedback'><span>{{ $message }}</span></div>
+                        @enderror
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="description">Descrição</label>
                         <input type="text" class="form-control" id="description" name="description"
                             placeholder="Delicioso X-Burguer">
+                        @error('description')
+                            <div class='invalid-feedback'><span>{{ $message }}</span></div>
+                        @enderror
                     </div>
 
                     <div class="mb-3">
@@ -33,12 +45,18 @@
                                 <option value={{ $category->id }}>{{ $category->name }}</option>
                             @endforeach
                         </select>
+                        @error('category')
+                            <div class='invalid-feedback'><span>{{ $message }}</span></div>
+                        @enderror
                     </div>
 
 
                     <div class="mb-3">
                         <label class="form-label" for="image">Imagem</label>
                         <input type="file" class="form-control" id="image" name="image">
+                        @error('image')
+                            <div class='invalid-feedback'><span>{{ $message }}</span></div>
+                        @enderror
                     </div>
 
                     <div class="modal-footer">
