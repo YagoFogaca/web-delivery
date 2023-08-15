73c9a5a6c8c9aa8167260018400722fad346a5c6
YagoFogaca <yagofogaca20@gmail.com>
2023-08-15 18:14:26 -0300

    feat: criação do schema e model de user

--- serve/app/Models/User.php
+++ serve/app/Models/User.php
@@ -20,8 +20,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'telephone',
+        'password'
     ];
+
+    protected $table = 'user';
 
     /**
      * The attributes that should be hidden for serialization.
---
+++ serve/database/migrations/2023_08_15_183656_create_address_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep');
+            $table->string('state')->nullable();
+            $table->string('city')->nullable();
+            $table->string('district')->nullable();
+            $table->string('street')->nullable();
+            $table->string('number_address');
+            $table->string('complement')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('address');
+    }
+};
